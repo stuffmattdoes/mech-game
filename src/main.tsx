860f6f67ed91f25130dce79880d72da44dce0271
Matthew Morrison <m.james.morrison00@gmail.com>
2023-10-29 13:51:44 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -237,39 +237,39 @@ function Plane() {
   </mesh>
 }
 
-function pixelCameraDolly(
-  camera: IOrthographicCamera,
-  aspectRatio: number,
-  pixelsPerScreenWidth: number,
-  pixelsPerScreenHeight: number
-) {
+// function pixelCameraDolly(
+//   camera: IOrthographicCamera,
+//   aspectRatio: number,
+//   pixelsPerScreenWidth: number,
+//   pixelsPerScreenHeight: number
+// ) {
 
-  // 0. Get Pixel Grid Units
-  const worldScreenWidth = ( ( camera.right - camera.left ) / camera.zoom );
-  const worldScreenHeight = ( ( camera.top - camera.bottom ) / camera.zoom );
-  const pixelWidth = worldScreenWidth / pixelsPerScreenWidth;
-  const pixelHeight = worldScreenHeight / pixelsPerScreenHeight;
+//   // 0. Get Pixel Grid Units
+//   const worldScreenWidth = ( ( camera.right - camera.left ) / camera.zoom );
+//   const worldScreenHeight = ( ( camera.top - camera.bottom ) / camera.zoom );
+//   const pixelWidth = worldScreenWidth / pixelsPerScreenWidth;
+//   const pixelHeight = worldScreenHeight / pixelsPerScreenHeight;
 
-  // 1. Project the current camera position along its local rotation bases
-  const camPos = new Vector3(); camera.getWorldPosition( camPos );
-  const camRot = new Quaternion(); camera.getWorldQuaternion( camRot );
-  const camRight = new Vector3( 1.0, 0.0, 0.0 ).applyQuaternion( camRot );
-  const camUp = new Vector3( 0.0, 1.0, 0.0 ).applyQuaternion( camRot );
-  const camPosRight = camPos.dot( camRight );
-  const camPosUp = camPos.dot( camUp );
+//   // 1. Project the current camera position along its local rotation bases
+//   const camPos = new Vector3(); camera.getWorldPosition( camPos );
+//   const camRot = new Quaternion(); camera.getWorldQuaternion( camRot );
+//   const camRight = new Vector3( 1.0, 0.0, 0.0 ).applyQuaternion( camRot );
+//   const camUp = new Vector3( 0.0, 1.0, 0.0 ).applyQuaternion( camRot );
+//   const camPosRight = camPos.dot( camRight );
+//   const camPosUp = camPos.dot( camUp );
 
-  // 2. Find how far along its position is along these bases in pixel units
-  const camPosRightPx = camPosRight / pixelWidth;
-  const camPosUpPx = camPosUp / pixelHeight;
+//   // 2. Find how far along its position is along these bases in pixel units
+//   const camPosRightPx = camPosRight / pixelWidth;
+//   const camPosUpPx = camPosUp / pixelHeight;
 
-  // 3. Find the fractional pixel units and convert to world units
-  const fractX = camPosRightPx - Math.round( camPosRightPx );
-  const fractY = camPosUpPx - Math.round( camPosUpPx );
+//   // 3. Find the fractional pixel units and convert to world units
+//   const fractX = camPosRightPx - Math.round( camPosRightPx );
+//   const fractY = camPosUpPx - Math.round( camPosUpPx );
 
-  // 4. Add fractional world units to the left/right top/bottom to align with the pixel grid
-  camera.left = - aspectRatio - ( fractX * pixelWidth );
-  camera.right = aspectRatio - ( fractX * pixelWidth );
-  camera.top = 1.0 - ( fractY * pixelHeight );
-  camera.bottom = - 1.0 - ( fractY * pixelHeight );
-  camera.updateProjectionMatrix();
-}
+//   // 4. Add fractional world units to the left/right top/bottom to align with the pixel grid
+//   camera.left = - aspectRatio - ( fractX * pixelWidth );
+//   camera.right = aspectRatio - ( fractX * pixelWidth );
+//   camera.top = 1.0 - ( fractY * pixelHeight );
+//   camera.bottom = - 1.0 - ( fractY * pixelHeight );
+//   camera.updateProjectionMatrix();
+// }
