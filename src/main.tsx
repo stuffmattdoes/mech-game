142f344a0479985e27b18ae52e80bd1124f863e6
Matthew Morrison <m.james.morrison00@gmail.com>
2023-10-29 22:53:13 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { NearestFilter, OrthographicCamera as IOrthographicCamera, RepeatWrapping, MOUSE } from 'three';
+import { NearestFilter, OrthographicCamera as IOrthographicCamera, RepeatWrapping, MOUSE, Vector3, Quaternion } from 'three';
 import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
 import { Html, OrbitControls, OrthographicCamera, StatsGl, useProgress, useTexture } from '@react-three/drei';
 import { EffectComposer } from '@react-three/postprocessing';
@@ -50,11 +50,10 @@ function Loader() {
 
 function Environment() {
   const { viewport } = useThree();
-  // const thr =  useThree();
 
-  // useFrame(({ camera, viewport }) => {
-  //   pixelCameraDolly(camera as IOrthographicCamera, viewport.aspect, 144, 120);
-  // });
+  useFrame(({ camera, viewport }) => {
+    pixelCameraDolly(camera as IOrthographicCamera, viewport.aspect, 144, 120);
+  });
 
 return <>
     <OrthographicCamera
@@ -108,8 +107,6 @@ return <>
 }
 
 function Scene() {
-  const texture = useTexture('textures/checker.png');
-
   return <scene>
     <Gem position={[0, 0.5, .25]}>
       <pointLight
@@ -250,39 +247,39 @@ function Plane({
   </mesh>
 }
 
-// function pixelCameraDolly(
-//   camera: IOrthographicCamera,
-//   aspectRatio: number,
-//   pixelsPerScreenWidth: number,
-//   pixelsPerScreenHeight: number
-// ) {
+function pixelCameraDolly(
+  camera: IOrthographicCamera,
+  aspectRatio: number,
+  pixelsPerScreenWidth: number,
+  pixelsPerScreenHeight: number
+) {
 
-//   // 0. Get Pixel Grid Units
-//   const worldScreenWidth = ( ( camera.right - camera.left ) / camera.zoom );
-//   const worldScreenHeight = ( ( camera.top - camera.bottom ) / camera.zoom );
-//   const pixelWidth = worldScreenWidth / pixelsPerScreenWidth;
-//   const pixelHeight = worldScreenHeight / pixelsPerScreenHeight;
+  // 0. Get Pixel Grid Units
+  const worldScreenWidth = ( ( camera.right - camera.left ) / camera.zoom );
+  const worldScreenHeight = ( ( camera.top - camera.bottom ) / camera.zoom );
+  const pixelWidth = worldScreenWidth / pixelsPerScreenWidth;
+  const pixelHeight = worldScreenHeight / pixelsPerScreenHeight;
 
-//   // 1. Project the current camera position along its local rotation bases
-//   const camPos = new Vector3(); camera.getWorldPosition( camPos );
-//   const camRot = new Quaternion(); camera.getWorldQuaternion( camRot );
-//   const camRight = new Vector3( 1.0, 0.0, 0.0 ).applyQuaternion( camRot );
-//   const camUp = new Vector3( 0.0, 1.0, 0.0 ).applyQuaternion( camRot );
-//   const camPosRight = camPos.dot( camRight );
-//   const camPosUp = camPos.dot( camUp );
+  // 1. Project the current camera position along its local rotation bases
+  const camPos = new Vector3(); camera.getWorldPosition( camPos );
+  const camRot = new Quaternion(); camera.getWorldQuaternion( camRot );
+  const camRight = new Vector3( 1.0, 0.0, 0.0 ).applyQuaternion( camRot );
+  const camUp = new Vector3( 0.0, 1.0, 0.0 ).applyQuaternion( camRot );
+  const camPosRight = camPos.dot( camRight );
+  const camPosUp = camPos.dot( camUp );
 
-//   // 2. Find how far along its position is along these bases in pixel units
-//   const camPosRightPx = camPosRight / pixelWidth;
-//   const camPosUpPx = camPosUp / pixelHeight;
+  // 2. Find how far along its position is along these bases in pixel units
+  const camPosRightPx = camPosRight / pixelWidth;
+  const camPosUpPx = camPosUp / pixelHeight;
 
-//   // 3. Find the fractional pixel units and convert to world units
-//   const fractX = camPosRightPx - Math.round( camPosRightPx );
-//   const fractY = camPosUpPx - Math.round( camPosUpPx );
+  // 3. Find the fractional pixel units and convert to world units
+  const fractX = camPosRightPx - Math.round( camPosRightPx );
+  const fractY = camPosUpPx - Math.round( camPosUpPx );
 
-//   // 4. Add fractional world units to the left/right top/bottom to align with the pixel grid
-//   camera.left = - aspectRatio - ( fractX * pixelWidth );
-//   camera.right = aspectRatio - ( fractX * pixelWidth );
-//   camera.top = 1.0 - ( fractY * pixelHeight );
-//   camera.bottom = - 1.0 - ( fractY * pixelHeight );
-//   camera.updateProjectionMatrix();
-// }
+  // 4. Add fractional world units to the left/right top/bottom to align with the pixel grid
+  camera.left = - aspectRatio - ( fractX * pixelWidth );
+  camera.right = aspectRatio - ( fractX * pixelWidth );
+  camera.top = 1.0 - ( fractY * pixelHeight );
+  camera.bottom = - 1.0 - ( fractY * pixelHeight );
+  camera.updateProjectionMatrix();
+}
